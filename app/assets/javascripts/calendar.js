eed4521770d693b23acd19d63dafc8c3f530e88c
Yasunori Kobayashi <aaa.ys40.y2@gmail.com>
2019-12-05 02:10:33 +0900

    [update] 表示修正

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,11 +1,28 @@
 $(document).on("turbolinks:load", function() {
-  $("#calendar").fullCalendar({
-    header: {
-      left: "prevYear,prev",
-      center: "title,today,month,agendaWeek,agendaDay,listYear",
-      right: "next,nextYear"
-    },
-    firstDay: 1
+  var data = new Array();
+  $.ajax({
+    type: "GET",
+    url: "/",
+    dataType: "json"
+  })
+  .done(function(event) {
+    $("#calendar").fullCalendar({
+      timeFormat: "HH:mm", // 24時間表記にする
+      header: {
+        left: "prev,next",
+        center: "title",
+        right: "today"
+      },
+      displayEventEnd: {
+        // 終了時間を表示
+        month: true,
+        basicWeek: false,
+        default: true
+      },
+      events: event // 受け取ったjsonの中身全部表示
+    });
+  })
+  .fail(function(event) {
+    alert("ng");
   });
-  
 });
