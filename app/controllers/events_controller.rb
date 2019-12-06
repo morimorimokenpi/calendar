99aa42a60dd8b370a93d360bdae823ca034b4b35
Yasunori Kobayashi <aaa.ys40.y2@gmail.com>
2019-12-06 21:44:28 +0900

    [add] 新規登録機能

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,12 +1,10 @@
 class EventsController < ApplicationController
-  def index
-    @events = Event.all
-    respond_to do |format|
-      format.html
-      format.json
-    end
-  end
+  before_action :set_event, only: [:destroy, :edit, :update]
 
+  def index
+    @events = Event.all.order("created_at DESC")
+  end
+  
   def new
     @event = Event.new
   end
@@ -20,7 +18,23 @@ class EventsController < ApplicationController
     end
   end
 
+  def edit
+    event = Event.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+    event = Event.find(params[:id])
+    redirect_to events_path if event.destroy
+  end
+
   private
+  def set_event
+    event = Event.find(params[:id])
+  end
+  
   def event_params
     params.require(:event).permit(:title, :start, :end)
   end
