9d78399eaab6635cac050227fd60b91763683923
Yasunori Kobayashi <aaa.ys40.y2@gmail.com>
2019-12-08 04:22:15 +0900

    [update] アソシエーションとバリデーション追加

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,8 @@
 class Event < ApplicationRecord
+  belongs_to :color
+
+  validates :title, presence: true
+  validates :start, presence: true
+  validates :end,   presence: true
+  validates :color_id, presence: true
 end
